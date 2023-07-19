6b60cd784b3f733de0c4c01dba463ff3db40e10b
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2023-07-19 23:06:28 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"go/scanner"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain,hasMX,hasSPF,sprRecord,hasDMARC,dmarcRecord\n")
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: could not read from input: %v\n", err)
+	}
+}
+
+func checkDomain(domain string) {
+
+}
